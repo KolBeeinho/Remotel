2e87be3edc89d1c3d07e8f59903d4a47210d1fa9
KolBeeinho <111688897+KolBeeinho@users.noreply.github.com>
2023-03-18 15:07:18 +0100

    Delete Remotel directory

--- Remotel/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "../styles/index.css";
-import type { AppProps } from "next/app";
-
-export default function Remotel({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
