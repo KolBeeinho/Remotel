d277c8e7f2d4bd93917e8b9a754120dbb35eddd1
KolBeeinho <111688897+KolBeeinho@users.noreply.github.com>
2023-04-22 13:18:15 +0200

    Delete Remotel directory

--- Remotel/public/locales/hu/Kapcsolat/contact.js
+++
@@ -1,7 +0,0 @@
-export default {
-  contacth2: "Kapcsolat",
-  contacth3: "Remotel",
-  contacth4: "Telefon",
-  contactp:
-    "Kérdése van keressen, ha takarítást szeretne szóljon, ha futni akar fusson",
-};
--- Remotel/public/locales/hu/Rólunk/aboutus.js
+++
@@ -1,6 +0,0 @@
-export default {
-  aboutusid: "rolunk",
-  aboutusheader: "Rólunk",
-  aboutusp:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-};
